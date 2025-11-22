4de9cf50f8d21c380a109b0ea5412d9dc3f539f7
ErgashevDjahongir <163175250+ErgashevDjahongir@users.noreply.github.com>
2025-11-22 12:11:02 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors');
 
-cons0t app = express();
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -40,3 +40,4 @@ app.delete('/api/notes/:id', async (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Backend listening on', port));
+fthgfhfghgfhfg
